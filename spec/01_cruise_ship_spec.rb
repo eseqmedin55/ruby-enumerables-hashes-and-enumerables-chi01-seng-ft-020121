8ca4ecde614935f9d43d9d4d55cbf2a0cd5323af
esequiel medina <medinaesequiel@gmail.com>
2021-01-20 01:07:48 +0000

    Automatically backed up by Learn

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/cruise_ship.rb"
+
 
 describe "#select_winner" do
   it "returns the name of the passenger who stays in suite a and whose name begins with the letter 'A'" do 
